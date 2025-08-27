4f6541703103c833182322a9419579081fe764e6
Borrascas <borrascasocasion@gmail.com>
2025-08-27 23:50:26 +0200

    Configurar PWA: manifest, service worker e iconos

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'borrascas-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
